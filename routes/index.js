f27d27584a8336b41bab6e07b3b5a3513d3e9937
Bruce Grugett <bgrugett@gmail.com>
2017-03-12 20:29:39 -0400

    new views

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/',function(req,res,next){
-  res.render("You made it!");
+  res.send("You made it!");
   ///add some stuff
 });
 
